6cf65185c068fa294155b55e638db643d059a96b
Penguuu <pengvang95@yahoo.com>
2023-05-22 22:36:38 -0500

    adding codes

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+const dbFilePath = path.join(__dirname, 'db.json');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading notes:', err);
+      res.status(500).json({ error: 'Unable to fetch notes' });
+    } else {
+      try {
+        const notes = JSON.parse(data);
+        res.json(notes);
+      } catch (error) {
+        console.error('Error parsing notes:', error);
+        res.status(500).json({ error: 'Unable to fetch notes' });
+      }
+    }
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (!title || !text) {
+    res.status(400).json({ error: 'Invalid data' });
+    return;
+  }
+
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Unable to fetch notes:', err);
+      res.status(500).json({ error: 'Failed to save note' });
+    } else {
+      try {
+        const notes = JSON.parse(data);
+        const newNote = { id: uuidv4(), title, text };
+        notes.push(newNote);
+
+        fs.writeFile(dbFilePath, JSON.stringify(notes), 'utf8', (err) => {
+          if (err) {
+            console.error('Error writing notes:', err);
+            res.status(500).json({ error: 'Failed to save note' });
+          } else {
+            res.json(newNote);
+          }
+        });
+      } catch (error) {
+        console.error('Error parsing notes:', error);
+        res.status(500).json({ error: 'Failed to save note' });
+      }
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ script.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const dbFilePath = path.join(__dirname, '..', 'db.json');
+
+function readNotesFromFile() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const notes = JSON.parse(data);
+          resolve(notes);
+        } catch (error) {
+          reject(error);
+        }
+      }
+    });
+  });
+}
+
+function writeNotesToFile(notes) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(dbFilePath, JSON.stringify(notes), 'utf8', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const saveButton = document.getElementById('save-note');
+
+  saveButton.addEventListener('click', () => {
+    const titleInput = document.getElementById('note-title');
+    const textInput = document.getElementById('note-text');
+
+    const title = titleInput.value;
+    const text = textInput.value;
+
+    if (title && text) {
+      // will read the exisint notes
+      readNotesFromFile()
+        .then((notes) => {
+          const newNote = {
+            id: uuidv4(),
+            title,
+            text,
+          };
+
+          // add notes to existing notes
+          notes.push(newNote);
+
+          // update note array
+          writeNotesToFile(notes)
+            .then(() => {
+              console.log('Note saved:', newNote);
+
+              // clears
+              titleInput.value = '';
+              textInput.value = '';
+            })
+            .catch((error) => {
+              console.error('Unable to saves notes:', error);
+            });
+        })
+        .catch((error) => {
+          console.error('Unable to read notes:', error);
+        });
+    }
+  });
+});
